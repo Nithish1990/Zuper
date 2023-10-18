d27df8126b0e4d5a25fac5d84bb726441b8a1543
Nithish Kumar S <shannithish2990@gmail.com>
2023-10-18 22:03:12 +0530

    Initial Commit

---
+++ Zuper/ZuperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuperApp.swift
+//  Zuper
+//
+//  Created by nithish-17632 on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ZuperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
